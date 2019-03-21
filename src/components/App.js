db8e02230feebaf489e4cb4b1c0eac6751b7337d
Jared Reando <jaredreando@gmail.com>
2019-03-21 15:13:02 -0700

    Add state to main report component. Move API fetch and parsing functions into componentDidMount class method.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import ReportList from './components/ReportList';
+import ReportList from './ReportList';
 
 class App extends Component {
   render() {
