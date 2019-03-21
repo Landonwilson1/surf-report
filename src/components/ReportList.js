754bdaf6f1fc2179cd125a38d2cb8367b7f43d71
Jared Reando <jaredreando@gmail.com>
2019-03-21 10:34:46 -0700

    Create react app setup and confirm first component displays as expected.

---
+++ src/components/ReportList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+class ReportList extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+render() {
+  return(
+    <div>
+      <h1>ReportList Works</h1>
+    </div>
+  )
+}
+
+}
+
+
+export default ReportList
